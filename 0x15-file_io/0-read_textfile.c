8422de88ab1941e507724a45aa724ce5af1da299
mihialx <mohammedibrahimhamid.cs+alxgithub@gmail.com>
2023-04-10 22:51:59 +0300

    fix commit and pushed

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,6 +1,13 @@
 #include "main.h"
 #include <stdlib.h>
 
+// Prototype: ssize_t read_textfile(const char *filename, size_t letters);
+// where letters is the number of letters it should read and print
+// returns the actual number of letters it could read and print
+// if the file can not be opened or read, return 0
+// if filename is NULL return 0
+// if write fails or does not write the expected amount of bytes, return 0
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	ssize_t o, r, w;
