8c61751068f5f054b131eb14f8d91a24d9d4d0cf
mihialx <mohammedibrahimhamid.cs+alxgithub@gmail.com>
2023-04-10 23:47:45 +0300

    fixed indent

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -16,8 +16,8 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t w;
 
 	o = open(filename, O_RDONLY);
-    if(o == -1)
-        return(0);
+	if (o == -1)
+		return (0);
 	buffer = malloc(sizeof(char) * letters);
 	r = read(o, buffer, letters);
 	w = write(STDOUT_FILENO, buffer, r);
